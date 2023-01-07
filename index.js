115a0626627acfc822dba2c6d72bca8d0f346e8c
jeremy klassen <jeremyklassen@jeremys-MacBook-Air.local>
2023-01-06 21:19:06 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// initial selectors and css
+document.body.style.backgroundColor = 'white';
+const main = document.getElementById('container');
+
+// creating containers inside main div
+const output = document.createElement('div');
+output.id('output');
+const input = document.createElement('div');
+input.id('input');
+
+// create buttons
+
+function generateTableHead(buttons) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+}
+
+const buttons = document.createElement('table');
+input.appendChild(buttons);
